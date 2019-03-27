ffae2cd23d017daae7981f51ffd592327b725c64
david martinez <david.martinezenciso@gmail.com>
2019-03-27 15:32:13 -0600

    Added files from another project by mistake

--- src/main/java/com/elatusdev/ssreader/model/ReadResult.java
+++ src/main/java/com/elatusdev/ssreader/model/ReadResult.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.elatusdev.model;
+package com.elatusdev.ssreader.model;
 
 import java.util.List;
 import java.util.Map;
